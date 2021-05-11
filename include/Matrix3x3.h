2e7f7785bb8f5086c07229374ef88ad8587e92c9
MateuszSzlachetko <mateusz.szlachetko@gmail.com>
2021-05-11 23:02:03 +0200

    Dokumentacja,Macierz 4x4 z modyfikacji

--- include/Matrix3x3.h
+++ include/Matrix3x3.h
@@ -1,11 +1,57 @@
 #ifndef MATRIX3X3_H
 #define MATRIX3X3_H
 
+/** @file 
+ *  @brief Specified Matrices
+ *  
+ * Typedef of Matrix3x3 and Matrix4x4
+ * 
+ * Functions for setting rotation matrix
+ * and for it's combination from modification
+*/
+
 #include "Matrix.h"
 
 typedef Matrix<double, 3> Matrix3x3;
 
+/** @fn void set_Rotation_OX(Matrix3x3 &m, double angle);
+ *  @brief Sets rotation matrix X
+ * 
+ * Using Euler formulas,and deg-rad calculation
+ * (X axis)
+ * @param m -matrix which will be manipulated
+ * @param angle -rotation angle in degrees
+ */
 void set_Rotation_OX(Matrix3x3 &m, double angle);
+
+/** @fn void set_Rotation_OY(Matrix3x3 &m, double angle);
+ *  @brief Sets rotation matrix Y
+ * 
+ * Using Euler formulas,and deg-rad calculation
+ * (Y axis)
+ * @param m -matrix which will be manipulated
+ * @param angle -rotation angle in degrees
+ */
 void set_Rotation_OY(Matrix3x3 &m, double angle);
+
+/** @fn void set_Rotation_OZ(Matrix3x3 &m, double angle);
+ *  @brief Sets rotation matrix Z
+ * 
+ * Using Euler formulas,and deg-rad calculation
+ * (Z axis)
+ * @param m -matrix which will be manipulated
+ * @param angle -rotation angle in degrees
+ */
 void set_Rotation_OZ(Matrix3x3 &m, double angle);
+
+#include "Vector3D.h"
+
+typedef Matrix<double, 4> Matrix4x4;
+
+/** @fn void RotXYZ_translate(Matrix4x4 &m, double a, double b, double g, Vector3D translation);
+ *  @brief Modification
+ * 
+ * General approach,all operations held in one 4x4 matrix.
+ */
+void RotXYZ_translate(Matrix4x4 &m, double a, double b, double g, Vector3D translation);
 #endif // MATRIX3X3_H
--- source/Matrix3x3.cpp
+++ source/Matrix3x3.cpp
@@ -24,3 +24,31 @@ void set_Rotation_OZ(Matrix3x3 &m, double angle)
 
     m = Matrix3x3({cos_a, -sin_a, 0, sin_a, cos_a, 0, 0, 0, 1});
 }
+
+void RotXYZ_translate(Matrix4x4 &m, double a, double b, double g, Vector3D translation)
+{
+    // calculate all angles
+    double sin_a, cos_a;
+    sin_a = sin(a * M_PI / 180);
+    cos_a = cos(a * M_PI / 180);
+
+    double sin_b, cos_b;
+    sin_b = sin(b * M_PI / 180);
+    cos_b = cos(b * M_PI / 180);
+
+    double sin_g, cos_g;
+    sin_g = sin(g * M_PI / 180);
+    cos_g = cos(g * M_PI / 180);
+
+    // set matrix from the formula
+
+    m = Matrix4x4({cos_a * cos_b, cos_a * sin_b * sin_g - sin_a * cos_g, cos_a * sin_b * cos_g + sin_a + sin_g, translation[0],
+                   sin_a * cos_b, sin_a * sin_b * sin_g + cos_a * cos_g, sin_a * sin_b * cos_g - cos_a * sin_g, translation[1],
+                   -sin_b, cos_b * sin_g, cos_b * cos_g, translation[2],
+                   0, 0, 0, 1});
+
+    // Show created matrix
+
+    std::cout << std::fixed;
+    std::cout << m << std::endl;
+}
